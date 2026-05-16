13174b965388d5366d3367b33e78e87ef9eaafa1
John Wilger <john@johnwilger.com>
2026-05-16 10:43:14 -0700

    Add Nix flake for Rust development

---
+++ flake.nix
@@ -0,0 +1,121 @@
+{
+  description = "Development environment and release builds for eddy";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+    };
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , fenix
+    , crane
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlays.default ];
+        };
+
+        rustToolchain = pkgs.fenix.complete.withComponents [
+          "cargo"
+          "clippy"
+          "rust-src"
+          "rustc"
+          "rustfmt"
+        ];
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+        commonBuildInputs = with pkgs; [
+          openssl
+          sqlite
+        ] ++ lib.optionals stdenv.isDarwin [
+          darwin.apple_sdk.frameworks.AppKit
+          darwin.apple_sdk.frameworks.CoreFoundation
+          darwin.apple_sdk.frameworks.CoreServices
+          darwin.apple_sdk.frameworks.Security
+          darwin.apple_sdk.frameworks.SystemConfiguration
+        ];
+
+        commonNativeBuildInputs = with pkgs; [
+          installShellFiles
+          pkg-config
+        ];
+
+        cargoToml = ./Cargo.toml;
+        hasCargoProject = builtins.pathExists cargoToml;
+
+        package = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
+          buildInputs = commonBuildInputs;
+          nativeBuildInputs = commonNativeBuildInputs;
+        };
+      in
+      {
+        packages = pkgs.lib.optionalAttrs hasCargoProject {
+          default = package;
+          eddy = package;
+        };
+
+        apps = pkgs.lib.optionalAttrs hasCargoProject {
+          default = flake-utils.lib.mkApp { drv = package; };
+          eddy = flake-utils.lib.mkApp { drv = package; };
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustToolchain
+            cargo-deny
+            cargo-mutants
+            cargo-nextest
+            cargo-watch
+            jq
+            just
+            openssl
+            pkg-config
+            sqlite
+          ] ++ lib.optionals stdenv.isLinux [
+            alsa-lib
+            fontconfig
+            libx11
+            libxcb
+            libxcursor
+            libxi
+            libxkbcommon
+            libxrandr
+            udev
+            wayland
+          ] ++ lib.optionals stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.AppKit
+            darwin.apple_sdk.frameworks.CoreFoundation
+            darwin.apple_sdk.frameworks.CoreServices
+            darwin.apple_sdk.frameworks.Security
+            darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+          OPENSSL_NO_VENDOR = "1";
+          PKG_CONFIG_PATH = pkgs.lib.makeSearchPath "lib/pkgconfig" commonBuildInputs;
+        };
+      });
+}
